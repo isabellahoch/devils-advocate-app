429f136886a439d06be4fc5c84dbee39d52bd63d
isabella hochschild <ihochschild@gmail.com>
2020-04-24 09:23:53 -0700

    Initial Commit

---
+++ devils-advocate/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  devils-advocate
+//
+//  Created by Isabella Hochschild on 4/24/20.
+//  Copyright © 2020 Isabella Hochschild. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ devils-advocate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  devils-advocate
+//
+//  Created by Isabella Hochschild on 4/24/20.
+//  Copyright © 2020 Isabella Hochschild. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
